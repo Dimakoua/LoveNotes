0790155b3449c692eaa4d6f68d38953fbc6fbd6d
Dmytro <dimakoua@gmail.com>
2023-09-16 05:58:57 +0300

    v1

--- src/screens/IdeaListScreen.js
+++ src/screens/IdeaListScreen.js
@@ -2,106 +2,51 @@ import React, { useState, useEffect } from 'react';
 import {
   View,
   Text,
-  FlatList,
-  TouchableOpacity,
+  Button,
   Modal,
   TextInput,
   StyleSheet,
   SafeAreaView,
 } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useTranslation } from 'react-i18next';
-import BackButton from '../components/BackButton';
-import { useIdea } from '../services/IdeaGenerator';
+import { Picker } from '@react-native-picker/picker';
+import PushNotification from 'react-native-push-notification';
 
-const IdeaListScreen = () => {
-  const { t } = useTranslation();
-  const { getIdeas, saveIdeas } = useIdea();
-
-  const [ideas, setIdeas] = useState([]);
+const NotificationScreen = () => {
+  const [message, setMessage] = useState('');
+  const [selectedTime, setSelectedTime] = useState(new Date());
+  const [interval, setInterval] = useState('daily');
   const [modalVisible, setModalVisible] = useState(false);
-  const [selectedIdea, setSelectedIdea] = useState(null);
-  const [newIdeaText, setNewIdeaText] = useState('');
 
   useEffect(() => {
-    loadIdeas();
+    PushNotification.configure({
+      onNotification: function (notification) {
+        console.log('Notification received:', notification);
+      },
+    });
   }, []);
 
-  const loadIdeas = async () => {
-    const storedIdeas = await getIdeas();
-    setIdeas(storedIdeas);
-  };
+  const scheduleNotification = () => {
+    const currentDate = new Date();
+    const date = new Date(currentDate.getTime() + 10000);
 
-  const addIdea = () => {
-    if (newIdeaText) {
-      const updatedIdeas = [...ideas, { id: Date.now(), text: newIdeaText, key: newIdeaText, done: false, like: false }];
-      setIdeas(updatedIdeas);
-      saveIdeas(updatedIdeas);
-      setNewIdeaText('');
-      setModalVisible(false);
-    }
-  };
+    console.log('Scheduled time:', date);
 
-  const editIdea = () => {
-    if (selectedIdea && newIdeaText) {
-      const updatedIdeas = ideas.map((idea) =>
-        idea.id === selectedIdea.id
-          ? { ...idea, text: newIdeaText }
-          : idea
-      );
-      setIdeas(updatedIdeas);
-      saveIdeas(updatedIdeas);
-      setNewIdeaText('');
-      setModalVisible(false);
-    }
-  };
-
-  const deleteIdea = () => {
-    if (selectedIdea) {
-      const updatedIdeas = ideas.filter(
-        (idea) => idea.id !== selectedIdea.id
-      );
-      setIdeas(updatedIdeas);
-      saveIdeas(updatedIdeas);
-      setModalVisible(false);
-    }
+    PushNotification.localNotificationSchedule({
+      id: '1',
+      title: 'Scheduled Notification',
+      message: message,
+      date: date,
+    });
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.backButton}>
-        <BackButton />
-      </View>
-      <Text style={styles.header}>{t('my_ideas')}</Text>
-      <FlatList
-        data={ideas}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            style={styles.ideaContainer}
-            onPress={() => {
-              setSelectedIdea(item);
-              setNewIdeaText(item.text);
-              setModalVisible(true);
-            }}
-          >
-            <Text style={styles.ideaText}>{t(item.key)}</Text>
-          </TouchableOpacity>
-        )}
-      />
+      <Text style={styles.header}>My Notifications</Text>
 
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={styles.addButton}
-          onPress={() => {
-            setSelectedIdea(null);
-            setNewIdeaText('');
-            setModalVisible(true);
-          }}
-        >
-          <Text style={styles.buttonText}>+</Text>
-        </TouchableOpacity>
-      </View>
+      <Button
+        title="Schedule Notification"
+        onPress={() => setModalVisible(true)}
+      />
 
       <Modal
         visible={modalVisible}
@@ -111,45 +56,35 @@ const IdeaListScreen = () => {
       >
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
+            <Text style={styles.modalHeader}>Schedule Notification</Text>
             <TextInput
               style={styles.input}
-              placeholder="Enter Your Idea"
-              onChangeText={(text) => setNewIdeaText(text)}
-              value={newIdeaText}
-              multiline={true}
+              placeholder="Enter your message"
+              onChangeText={(text) => setMessage(text)}
+              value={message}
             />
-            <View style={styles.buttonContainer}>
-              <TouchableOpacity
-                style={[styles.modalButton, styles.saveButton]}
-                onPress={selectedIdea ? editIdea : addIdea}
-              >
-                <Text style={styles.modalButtonText}>
-                  {selectedIdea ? 'Save' : 'Add'}
-                </Text>
-              </TouchableOpacity>
-              {selectedIdea && (
-                <TouchableOpacity
-                  style={[styles.modalButton, styles.deleteButton]}
-                  onPress={deleteIdea}
-                >
-                  <Text style={styles.modalButtonText}>{t('Delete')}</Text>
-                </TouchableOpacity>
-              )}
-              <TouchableOpacity
-                style={[styles.modalButton, styles.closeButton]}
-                onPress={() => {
-                  setModalVisible(false);
-                  setSelectedIdea(null);
-                  setNewIdeaText('');
-                }}
-              >
-                <Text style={styles.modalButtonText}>{t('Close')}</Text>
-              </TouchableOpacity>
-            </View>
+            <Picker
+              selectedValue={interval}
+              onValueChange={(itemValue, itemIndex) => setInterval(itemValue)}
+              style={styles.picker}
+            >
+              <Picker.Item label="Daily" value="daily" />
+              <Picker.Item label="Weekly" value="weekly" />
+              <Picker.Item label="Monthly" value="monthly" />
+            </Picker>
+
+            <Button title="Save" onPress={scheduleNotification} />
+            <Button
+              title="Close"
+              onPress={() => {
+                setModalVisible(false);
+                setMessage('');
+                setSelectedTime(new Date());
+              }}
+            />
           </View>
         </View>
       </Modal>
-
     </SafeAreaView>
   );
 };
@@ -158,101 +93,45 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(253, 246, 238, 0.7)', // Same background color as SettingsScreen
-  },
-  backButton: {
-    position: 'absolute',
-    top: 20,
-    left: 20,
+    backgroundColor: 'rgba(253, 246, 238, 0.7)',
   },
   header: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 16,
-    color: 'rgb(222, 178, 150)', // Same header color as SettingsScreen
+    color: 'rgb(222, 178, 150)',
   },
-  ideaContainer: {
-    backgroundColor: 'rgba(255, 204, 204, 0.7)', // Same background color as SettingsScreen
-    padding: 16,
-    borderRadius: 8,
-    marginBottom: 16,
-  },
-  ideaText: {
-    fontSize: 18,
-    color: 'rgb(51, 51, 51)', // Same text color as SettingsScreen
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-  },
-  addButton: {
-    backgroundColor: 'rgb(222, 178, 150)', // Same button background color as SettingsScreen
-    width: 54, // Set a fixed width and height to make it circular
-    height: 54,
-    borderRadius: 32, // Half of the width/height to create a circle
-    justifyContent: 'center', // Center the text vertically
-    alignItems: 'center', // Center the text horizontally
-    position: 'absolute', // Position it absolutely
-    bottom: 16, // Adjust the bottom position for spacing
-    right: 16, // Adjust the left position for spacing
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 24,
-  },
-
-
-
   modalContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
   modalContent: {
-    width: '90%', // Adjust the width as needed
-    backgroundColor: 'white', // White modal background
+    width: '90%',
+    backgroundColor: 'white',
     borderRadius: 8,
     padding: 20,
   },
-  input: {
-    width: '100%',
-    padding: 16,
-    borderColor: '#ddd', // Light gray border color
-    borderWidth: 1,
-    borderRadius: 5,
+  modalHeader: {
+    fontSize: 18,
+    fontWeight: 'bold',
     marginBottom: 16,
   },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    marginBottom: 16,
+    fontSize: 16,
   },
-  modalButton: {
-    flex: 1,
-    padding: 12,
-    borderRadius: 5,
-    marginHorizontal: 8,
-    alignItems: 'center',
-  },
-  modalButtonText: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  saveButton: {
-    backgroundColor: 'rgb(222, 178, 150)', // Save button background color
-  },
-  deleteButton: {
-    backgroundColor: '#FF5A5F', // Delete button background color
-  },
-  closeButton: {
-    backgroundColor: 'gray', // Close button background color
+  picker: {
+    borderWidth: 1,
+    borderColor: 'rgb(150, 178, 222)',
+    borderRadius: 8,
+    marginBottom: 16,
   },
 });
 
-
-export default IdeaListScreen;
+export default NotificationScreen;
