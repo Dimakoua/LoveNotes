a3b899ad52bc485de75dd8841a02bc1afe52bec8
Dmytro <dimakoua@gmail.com>
2023-11-05 03:40:26 +0200

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,15 @@ function App() {
   const createNotificationChannels = () => {
     PushNotification.createChannel(
       {
-        channelId: "defaultLocalPushesChannelId", // (required)
-        channelName: "defaultLocalPushesChannelName", // (required)
-        channelDescription: "Channel for local pushes", // (optional) default: undefined.
-        playSound: true, // (optional) default: true
-        soundName: "default", // (optional) See `soundName` parameter of `localNotification` function
-        importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
-        vibrate: true, // (optional) default: true. Creates the default vibration pattern if true.
+        channelId: "defaultLocalPushesChannelId",
+        channelName: "defaultLocalPushesChannelName",
+        channelDescription: "Channel for local pushes",
+        playSound: true,
+        soundName: "default", 
+        importance: Importance.HIGH,
+        vibrate: true,
       },
-      (created: any) => console.log(`createChannel returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
+      (created: any) => (created)
     );
   };
 
